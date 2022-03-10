e252330aced4432c7f138533a4014180dda18223
TomeCarvalho <tomecarvalho@ua.pt>
2022-03-10 15:55:11 +0000

    Lab1.2a

---
+++ lab1/lab1_2/src/test/java/euromillions/DipTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package euromillions;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author ico0
+ */
+public class DipTest {
+
+    private Dip instance;
+
+
+    public DipTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        instance = new Dip(new int[]{10, 20, 30, 40, 50}, new int[]{1, 2});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        instance = null;
+    }
+
+
+    @Test
+    public void testConstructorFromBadArrays() {
+        assertThrows(IllegalArgumentException.class, () -> new Dip(new int[]{}, new int[]{}));
+    }
+
+    @Test
+    public void testFormat() {
+        // note: correct the implementation of the format(), not the test...
+        String result = instance.format();
+        assertEquals("N[ 10 20 30 40 50] S[  1  2]", result, "format as string: formatted string not as expected. ");
+    }
+
+}
