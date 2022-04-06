793776588fc866c392ee2113949507e2944e8027
TomeCarvalho <tomecarvalho@ua.pt>
2022-04-06 19:53:26 +0100

    HW1: Initialise Spring Boot project

---
+++ HW1/covid/src/test/java/tqs/n97939/covid/CovidApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.n97939.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
