dced9f5189935aed7d66061ba3e12633a3944213
TomeCarvalho <tomecarvalho@ua.pt>
2022-04-07 16:31:09 +0100

    5.3

--- lab5/lab5_3/webautomation/src/test/java/io/github/bonigarcia/BlazeDemoSteps.java
+++ lab5/lab5_3/webautomation/src/test/java/io/github/bonigarcia/BlazeDemoSteps.java
@@ -1,3 +1,5 @@
+package io.github.bonigarcia;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -6,8 +8,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-public class MyStepdefs {
+
+public class BlazeDemoSteps {
     private WebDriver driver;
 
     @When("I navigate to {string}")
@@ -35,5 +40,33 @@ public class MyStepdefs {
     public void clickFindFlights() {
         driver.findElement(By.cssSelector(".form-inline:nth-child(4) > option:nth-child(3)")).click();
         driver.findElement(By.cssSelector(".btn-primary")).click();
+    }
+
+    @And("I click Choose This Flight")
+    public void iClickChooseThisFlight() {
+        driver.findElement(By.cssSelector("tr:nth-child(3) .btn")).click();
+    }
+
+
+    @And("I write {string} as my {string}")
+    public void iWriteAsMy(String val, String id) {
+        driver.findElement(By.id(id)).click();
+        driver.findElement(By.id(id)).sendKeys(val);
+    }
+
+    @And("I select {string} as my {string}")
+    public void iSelectAsMy(String arg0, String arg1) {
+        driver.findElement(By.cssSelector(".control-group:nth-child(9) > .control-label")).click();
+    }
+
+
+    @And("I click Purchase Flight")
+    public void iClickPurchaseFlight() {
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+    }
+
+    @Then("I should see my receipt")
+    public void iShouldSeeMyReceipt() {
+        assertThat(driver.getTitle(), is("BlazeDemo Confirmation"));
     }
 }
--- lab5/lab5_3/webautomation/src/test/resources/io/github/bonigarcia/login.feature
+++ lab5/lab5_3/webautomation/src/test/resources/io/github/bonigarcia/login.feature
@@ -5,15 +5,15 @@ Feature: BlazeDemo flight purchase
     And I select a flight from "Boston" to "London"
     And I click Find Flights
     And I click Choose This Flight
-    And I write "John Darksoul" as my "Name"
-    And I write "High Wall of Lothric" as my "Address"
-    And I write "Lothric Castle" as my "City"
-    And I write "Hollow" as my "State"
-    And I write "12345" as my "Zip Code"
-    And I select "American Express" as my "Credit Card Type"
-    And I write "123" as my "Credit Card Number"
-    And I write "03" as my "Month"
-    And I write "2016" as my "Year"
-    And I write "Chosen Undead" as my "Name on Card"
+    And I write "John Darksoul" as my "inputName"
+    And I write "High Wall of Lothric" as my "address"
+    And I write "Lothric Castle" as my "city"
+    And I write "Hollow" as my "state"
+    And I write "12345" as my "zipCode"
+    And I select "American Express" as my "cardType"
+    And I write "123" as my "creditCardNumber"
+    And I write "03" as my "creditCardMonth"
+    And I write "2016" as my "creditCardYear"
+    And I write "Chosen Undead" as my "nameOnCard"
     And I click Purchase Flight
     Then I should see my receipt
