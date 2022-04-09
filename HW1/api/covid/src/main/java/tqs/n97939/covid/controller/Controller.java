8f91c816909f27b31a1a9338fe90b15f4fd24169
TomeCarvalho <tomecarvalho@ua.pt>
2022-04-09 17:38:30 +0100

    Document API (springdoc-openapi)

--- HW1/api/covid/src/main/java/tqs/n97939/covid/controller/Controller.java
+++ HW1/api/covid/src/main/java/tqs/n97939/covid/controller/Controller.java
@@ -1,5 +1,8 @@
 package tqs.n97939.covid.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +23,35 @@ public class Controller {
     @Autowired
     private CovidService service;
 
+    @Operation(summary = "Get all countries/regions.")
+    @ApiResponse(responseCode = "200", description = "Got all countries/regions.")
     @GetMapping("/countries")
-    public ResponseEntity<String> getCountries(@RequestParam(required = false) String search) {
+    public ResponseEntity<String> getCountries(@Parameter(description = "Partial name search query.") @RequestParam(required = false) String search) {
         logger.info("GET /countries - search = " + search);
         return service.getCountries(search);
     }
 
+    @Operation(summary = "Get a country/region's current COVID-19 statistics.")
+    @ApiResponse(responseCode = "200", description = "Got all of the country/region's COVID-19 statistics.")
     @GetMapping("/statistics")
-    public ResponseEntity<String> getCurrentStatistics(@RequestParam(required = false) String country) {
+    public ResponseEntity<String> getCurrentStatistics(@Parameter(description = "Name of the country to be queried.") @RequestParam(required = false) String country) {
         logger.info("GET /statistics - country = " + country);
         return service.getCurrentStatistics(country);
     }
 
+
+    @Operation(summary = "Get a country/region's historical COVID-19 statistics (all or just a day's).")
+    @ApiResponse(responseCode = "200", description = "Got all of the country/region's desired historical COVID-19 statistics.")
     @GetMapping("/history")
-    public ResponseEntity<String> getHistory(@RequestParam String country, @RequestParam(required = false) String day) {
+    public ResponseEntity<String> getHistory(
+            @Parameter(description = "Name of the country to be queried.") @RequestParam String country,
+            @Parameter(description = "Date to be queried, in ISO 8601 format.") @RequestParam(required = false) String day) {
         logger.info("GET /history - country = " + country + ", day = " + day);
         return service.getHistory(country, day);
     }
 
+    @Operation(summary = "Get cache statistics.")
+    @ApiResponse(responseCode = "200", description = "Got cache statistics.")
     @GetMapping("/cache-stats")
     public ResponseEntity<String> getCacheStats() {
         return service.getCacheStats();
