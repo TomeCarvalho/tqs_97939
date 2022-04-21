d3a35a7301a605ab9719dab61ed57311f8da8379
TomeCarvalho <tomecarvalho@ua.pt>
2022-04-21 21:11:33 +0100

    I hate imports (7.3 done)

--- lab7/lab7_3/src/test/java/n97939/lab7_3/IntegrationTest.java
+++ lab7/lab7_3/src/test/java/n97939/lab7_3/IntegrationTest.java
@@ -1,27 +1,34 @@
 package n97939.lab7_3;
 
+import lombok.extern.slf4j.Slf4j;
 import n97939.lab7_3.data.Employee;
 import n97939.lab7_3.data.EmployeeRepository;
-import org.junit.Test;
+import n97939.lab7_3.util.JsonUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Lab73Application.class)
 @TestPropertySource(properties = "spring.jpa.hibernate.ddl-auto=create")
+@AutoConfigureMockMvc
+@Slf4j
 public class IntegrationTest {
     @Container
     public static PostgreSQLContainer container = new PostgreSQLContainer("postgres:12")
@@ -36,6 +43,9 @@ public class IntegrationTest {
     @Autowired
     private EmployeeRepository repository;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", container::getJdbcUrl);
@@ -47,6 +57,8 @@ public class IntegrationTest {
     public void setUpTestEmployees() throws Exception {
         jim = repository.save(new Employee("Jim Halpert", "jim@dundermifflin.com"));
         creed = repository.save(new Employee("Creed Bratton", "creed@dundermifflin.com"));
+        log.info("here");
+        System.out.println(repository.findAll().size());
     }
 
     @Test
@@ -58,7 +70,10 @@ public class IntegrationTest {
                 .pathSegment("api", "employees")
                 .build()
                 .toUriString();
-        repository.save(new Employee("Pam Beesly", "pam@dundermifflin.com"));
-        assertThat(repository.findAll().size(), equalTo(3));
+        Employee pam = new Employee("Pam Beesly", "pam@dundermifflin.com");
+        mockMvc.perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.toJson(pam)))
+                .andExpect(status().isCreated());
     }
 }
